69697f5979aff22d82dae9dd88a5ed093e7288ce
Aryan <jadhav.aryan0702@gmail.com>
2025-07-09 21:11:27 +0530

    Initial commit: Added QR code generator and requirements

---
+++ main.py
@@ -0,0 +1,21 @@
+import qrcode
+
+data = input("Enter the text or URL to encode in a QR Code:")
+
+qr = qrcode.QRCode(
+    version = 1,
+    box_size = 15, #15x15 pixels per box
+    border = 5
+)
+
+# Add the input data to the QR image
+qr.add_data(data)
+qr.make(fit = True)
+
+#create the QR image
+img = qr.make_image(fill_color = "Black", back_color = "White") 
+filename = "qr_code.png"
+img.save(filename)      #save the QR image 
+
+#confirm QR generation
+print(f"QR Code generated and saved as {filename}")
